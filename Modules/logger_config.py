8e962f6fff35961ff7ff189df7a8d4faf663e927
DPlll <160452879+DPlll@users.noreply.github.com>
2024-09-21 00:56:51 -0400

    added envtemplate

--- Modules/logger_config.py
+++ Modules/logger_config.py
@@ -1,41 +1,36 @@
 import os
 import logging 
 import colorlog
-import re
+
 
 # Sets up and configures the logger to output to both a file and the terminal with colors.
 def configure_logger(log_file='logs/crypto_analysis.log', log_level=logging.INFO):
 
-    # Create a logger
+     # Create a logger
     logger = logging.getLogger()
     logger.setLevel(log_level)
-    
+
     # Remove any existing handlers to avoid duplication
     if logger.hasHandlers():
         logger.handlers.clear()
-    
-    # Create a file handler for logging to a file
+
+    # File handler for logging to a file
     file_handler = logging.FileHandler(log_file, mode='w')
     file_handler.setLevel(log_level)
-    
-    # Create a console handler for colored output to the terminal
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    # Console handler for colored output
     console_handler = colorlog.StreamHandler()
     console_handler.setLevel(log_level)
-
-    # Define the log format & colors for file and console 
-    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     console_formatter = colorlog.ColoredFormatter(
-        '%(white)s%(asctime)s - %(bold)s%(log_color)s%(levelname)s%(reset)s - %(white)s%(message)s',
+        '%(asctime)s - %(bold)s%(log_color)s%(levelname)s%(reset)s - %(white)s%(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
-    
-    # Set the format for both handlers
-    file_handler.setFormatter(file_formatter)
     console_handler.setFormatter(console_formatter)
-    
+
     # Add handlers to the logger
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
-    
-    return logger
 
+    return logger
